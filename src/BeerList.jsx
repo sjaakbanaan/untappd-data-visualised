273ac5078664b8fd2ea4d8ca880f7d4fd8a5f44f
David Tuk <david@gohike.nl>
2024-01-23 08:53:58 +0100

    Working on new style for BeerList

--- src/BeerList.jsx
+++ src/BeerList.jsx
@@ -3,16 +3,23 @@ import { formatDate } from './utils';
 const BeerList = ({ beerData }) => {
   return (
     <div>
-      <ul className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <ul className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4  xl:grid-cols-6">
         {beerData?.length > 0 &&
           beerData.map((item) => (
-            <li key={item.checkin_id} className="bg-white rounded-lg p-4 shadow-md">
-              <h2 className="text-xl font-semibold mb-2">{item.beer_name}</h2>
-              <img src={item.photo_url} alt="" />
-              <p className="text-gray-700 mb-2">{item.brewery_name}</p>
-              <p className="text-gray-600">Created at: {formatDate(item.created_at)}</p>
+            <li
+              key={item.checkin_id}
+              className="bg-white rounded-lg p-4 shadow-md bg-cover"
+              style={{
+                backgroundColor: 'rgba(40, 40, 40, 1)',
+                backgroundImage: `url(${item.photo_url})`,
+              }}
+            >
+              <h2 className="text-xl text-white font-semibold mb-2">{item.beer_name}</h2>
+              {/* <img src={item.photo_url} alt="" /> */}
+              <p className="text-white mb-2">{item.brewery_name}</p>
+              <p className="text-white">Created at: {formatDate(item.created_at)}</p>
               <p className="text-green-600">Rating: {item.rating_score}</p>
-              <p className="text-gray-600">Venue: {item.venue_name}</p>
+              <p className="text-white">@{item.venue_name}</p>
             </li>
           ))}
       </ul>
