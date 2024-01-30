50584da142d1e5844e09038fbeb3f3fe6ba9363f
David Tuk <david@gohike.nl>
2024-01-30 22:02:10 +0100

    bugfix

--- set_home.js
+++ set_home.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 
+// the config file
+const config = require('./public/home-config.json');
+const newValues = config.home;
+
 // Load your original JSON file
 const originalData = require('./public/beers.json');
 
 // Create a Set to keep track of unique checkin_ids
 const uniqueCheckinIds = new Set();
 
+// Update the values in the original data based on the condition
+const updatedData = originalData.map((item) => {
+  if (item.venue_name === 'Untappd at Home') {
+    return {
+      ...item,
+      ...newValues,
+    };
+  }
+  return item;
+});
+
 // Filter out duplicates based on the 'checkin_id'
-const updatedData = originalData.filter((item) => {
+const filteredData = updatedData.filter((item) => {
   if (!uniqueCheckinIds.has(item.checkin_id)) {
     uniqueCheckinIds.add(item.checkin_id);
     return true;
@@ -15,7 +30,7 @@ const updatedData = originalData.filter((item) => {
   return false;
 });
 
-// Save the updated data to a new JSON file
+// Save the updated and filtered data to a new JSON file
 const outputFile = './public/beers-processed.json';
-fs.writeFileSync(outputFile, JSON.stringify(updatedData, null, 2));
-console.log('Duplicates removed. Updated data saved to', outputFile);
+fs.writeFileSync(outputFile, JSON.stringify(filteredData, null, 2));
+console.log('Values updated and duplicates removed. Updated data saved to', outputFile);
