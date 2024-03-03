fa86d61a4e2a214edab1c215249ad8b1af2d0455
David Tuk <david@gohike.nl>
2024-03-03 18:56:46 +0100

    fixed broken 'beers per year' chart

--- src/utils/getBarChartYearData.js
+++ src/utils/getBarChartYearData.js
@@ -1,7 +1,7 @@
 export const getBarChartYearData = (beerData) => {
   const beersPerYear = {};
   beerData.forEach((beer) => {
-    const year = new Date(beer.created_at).toString();
+    const year = new Date(beer.created_at).getFullYear().toString();
     if (!beersPerYear[year]) {
       beersPerYear[year] = 1;
     } else {
