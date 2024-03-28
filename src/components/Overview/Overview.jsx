c5404f1477cd4ba80cd96b7fc87db098b3a5e938
David Tuk <david@gohike.nl>
2024-03-28 21:35:57 +0100

    more cleanup

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import OverviewCardPropery from '../Overview/OverviewCardPropery.jsx';
+import OverviewCard from './OverviewCard.jsx';
 
 const Overview = ({ beerData }) => {
   const [showOverview, setShowOverview] = useState(false);
@@ -25,58 +25,7 @@ const Overview = ({ beerData }) => {
       )}
       {(showOverview || beerData?.length <= buttonNumber) && (
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-8">
-          {beerData?.length > 0 &&
-            beerData.map((item) => (
-              <div
-                key={item.checkin_id}
-                style={{
-                  backgroundImage: `url(${item.photo_url})`,
-                }}
-                className="block shadow-md bg-cover transition-transform duration-300 transform hover:scale-110 rounded-lg overflow-hidden"
-              >
-                <a
-                  href={item.checkin_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="p-4 pb-8 block bg-gray-800 min-h-96 h-full rounded-lg bg-opacity-50"
-                >
-                  <h2 className="text-xl text-white font-semibold mb-2">
-                    {item.beer_name}
-                  </h2>
-                  <p className="mb-4">{item.brewery_name}</p>
-                  <OverviewCardPropery
-                    icon="BEER"
-                    viewBox="0 0 473.639 512"
-                    value={item.beer_type}
-                  />
-                  <OverviewCardPropery
-                    icon="CALENDAR"
-                    viewBox="0 0 488 512"
-                    value={item.created_at}
-                    formatOutput
-                  />
-                  {item.venue_name && (
-                    <OverviewCardPropery
-                      icon="LOCATION"
-                      viewBox="0 0 70.749 90"
-                      value={item.venue_name}
-                    />
-                  )}
-                  <OverviewCardPropery
-                    icon="STAR"
-                    viewBox="0 0 512 512"
-                    value={`${item.rating_score} / ${item.global_rating_score}`}
-                  />
-                  {item.tagged_friends && (
-                    <OverviewCardPropery
-                      icon="FRIENDS"
-                      viewBox="0 0 512 398.108"
-                      value={item.tagged_friends.split(',').join(', ')}
-                    />
-                  )}
-                </a>
-              </div>
-            ))}
+          {beerData?.length > 0 && beerData.map((item) => <OverviewCard item={item} />)}
         </div>
       )}
     </>
---
+++ src/components/Overview/OverviewCard.jsx
@@ -0,0 +1,57 @@
+import OverviewCardProperty from './OverviewCardProperty.jsx';
+
+const OverviewCard = ({ item }) => {
+  return (
+    <div
+      key={item.checkin_id}
+      style={{
+        backgroundImage: `url(${item.photo_url})`,
+      }}
+      className="block shadow-md bg-center bg-cover transition-transform duration-500 transform hover:scale-110 rounded-lg overflow-hidden"
+    >
+      <a
+        href={item.checkin_url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="p-4 pb-8 block bg-gray-800 min-h-96 h-full rounded-lg bg-opacity-50"
+      >
+        <h2 className="text-xl text-white font-semibold mb-2">{item.beer_name}</h2>
+        <p className="mb-4">{item.brewery_name}</p>
+        <OverviewCardProperty
+          icon="BEER"
+          viewBox="0 0 473.639 512"
+          value={item.beer_type}
+        />
+        <OverviewCardProperty
+          icon="CALENDAR"
+          viewBox="0 0 488 512"
+          value={item.created_at}
+          formatOutput
+        />
+        {item.venue_name && (
+          <OverviewCardProperty
+            icon="LOCATION"
+            viewBox="0 0 70.749 90"
+            value={item.venue_name}
+          />
+        )}
+        {item.rating_score && (
+          <OverviewCardProperty
+            icon="STAR"
+            viewBox="0 0 512 512"
+            value={`${item.rating_score} / ${item.global_rating_score}`}
+          />
+        )}
+        {item.tagged_friends && (
+          <OverviewCardProperty
+            icon="FRIENDS"
+            viewBox="0 0 512 398.108"
+            value={item.tagged_friends.split(',').join(', ')}
+          />
+        )}
+      </a>
+    </div>
+  );
+};
+
+export default OverviewCard;
