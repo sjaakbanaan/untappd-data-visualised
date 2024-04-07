ee08778d9cdd4e2930487e18d3743c4743ab4fab
David Tuk <david@gohike.nl>
2024-04-07 21:05:50 +0200

    replaced the 'upload successful' notification with a redirect to the dashboard, saves you a click!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { DataContext } from './DataContext';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import Uploader from './components/Uploader/Uploader.jsx';
 
 const App = () => {
+  const { beerData } = useContext(DataContext);
   return (
     <Router>
       <div className="bg-gray-900 p-8">
         <div className="container relative mx-auto text-white">
           <Header />
           <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route
+              path="/"
+              element={beerData && beerData.length > 0 ? <Dashboard /> : <Uploader />}
+            />
             <Route path="/upload" element={<Uploader />} />
           </Routes>
           <Footer />
