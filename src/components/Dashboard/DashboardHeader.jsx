88459330a5028c3141495ec826fc6b2a09e0e6d3
David Tuk <david@gohike.nl>
2024-06-15 20:12:15 +0200

    better text in header

--- src/components/Dashboard/DashboardHeader.jsx
+++ src/components/Dashboard/DashboardHeader.jsx
@@ -5,7 +5,8 @@ const DashboardHeader = ({ filterOverview, setFilterOverview, totalBeerCount }) 
   return (
     <div className="md:flex md:items-center mt-10 md:mb-6">
       <h2 className="text-2xl font-bold">
-        {totalBeerCount} beers <span className="text-gray-400">in current selection</span>
+        {totalBeerCount} checkins{' '}
+        <span className="text-gray-400">in current selection</span>
       </h2>
       {isFilterOverviewSet(filterOverview) && (
         <ResetFilters setFilterOverview={setFilterOverview} />
